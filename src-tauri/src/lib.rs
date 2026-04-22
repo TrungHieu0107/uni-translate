c6fe5c1ee98722744cf80e2ab78da3aee6049243
trunghieu0107 <ntrunghieu1701@gmail.com>
2026-04-22 23:20:08 +0700

    feat(tauri): Add Tauri window state persistence plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -19,6 +19,7 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .manage(AppStateWrapper(Mutex::new(AppState::new())))
         .setup(|app| {
             let app_handle = app.handle().clone();
